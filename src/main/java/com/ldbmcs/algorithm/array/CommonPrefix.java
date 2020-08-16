de9b9c23efa895ab86819292730a69ae1f259a25
ldbmcs <ldbmcs@gmail.com>
2020-08-16 12:17:08 +0800

    反转字符串

---
+++ src/main/java/com/ldbmcs/algorithm/array/CommonPrefix.java
@@ -0,0 +1,15 @@
+package com.ldbmcs.algorithm.array;
+
+/**
+ * 最长公共前缀：https://leetcode-cn.com/problems/longest-common-prefix/
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 输入: ["flower","flow","flight"] 输出: "fl"
+ * 输入: ["dog","racecar","car"] 输出: ""  输入不存在公共前缀。
+ *
+ * @author ldbmcs
+ * @date 2020/8/16
+ */
+public class CommonPrefix {
+
+}
---
+++ src/main/java/com/ldbmcs/algorithm/string/ReverseString.java
@@ -0,0 +1,28 @@
+package com.ldbmcs.algorithm.string;
+
+/**
+ * 反转字符串:https://leetcode-cn.com/problems/reverse-string/
+ * 输入：["h","e","l","l","o"] 输出：["o","l","l","e","h"]
+ *
+ * @author ldbmcs
+ * @date 2020/8/16
+ */
+public class ReverseString {
+    public static void reverse(char[] s) {
+        int start = 0, end = s.length - 1;
+        char tmp;
+        while (start < end) {
+            tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            start++;
+            end--;
+        }
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverse(s);
+    }
+}
