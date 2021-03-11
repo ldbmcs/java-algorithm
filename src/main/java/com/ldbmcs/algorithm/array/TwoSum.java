906dd490803970c993a8f4a4b40f2e09676fac1f
dengcs <ldbmcs@gmail.com>
2021-03-11 21:05:30 +0800

    新增：两数之和

---
+++ src/main/java/com/ldbmcs/algorithm/array/TwoSum.java
@@ -0,0 +1,47 @@
+package com.ldbmcs.algorithm.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那两个整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * 你可以按任意顺序返回答案。
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ *
+ */
+public class TwoSum {
+
+//    public static int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return new int[]{0, 0};
+//    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] res = twoSum(nums, target);
+        System.out.println(Arrays.toString(res));
+    }
+}
