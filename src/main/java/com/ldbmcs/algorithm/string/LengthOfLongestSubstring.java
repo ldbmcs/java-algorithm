6be619c5f5048439763189bfeb0d843b36fa26ea
dengcs <ldbmcs@gmail.com>
2021-03-11 22:41:06 +0800

    新增：不重复字符最长子串的长度

--- src/main/java/com/ldbmcs/algorithm/string/LengthOfLongestSubstring.java
+++ src/main/java/com/ldbmcs/algorithm/string/LengthOfLongestSubstring.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串的长度。
- *
+ * <p>
  * 输入: s = "abcabcbb"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
@@ -18,17 +18,19 @@ public class LengthOfLongestSubstring {
         int res = 0;
         String[] charArray = s.split("");
         for (int i = 0; i < charArray.length; i++) {
-            List<String> resArray = new ArrayList<>();
-            resArray.add(charArray[i]);
-            for (int j = i + 1; j < charArray.length; j++) {
-                if (resArray.contains(charArray[j])) {
-                    break;
-                } else {
-                    resArray.add(charArray[j]);
+            if (charArray.length - i > res) {
+                List<String> resArray = new ArrayList<>();
+                resArray.add(charArray[i]);
+                for (int j = i + 1; j < charArray.length; j++) {
+                    if (resArray.contains(charArray[j])) {
+                        break;
+                    } else {
+                        resArray.add(charArray[j]);
+                    }
                 }
-            }
-            if (resArray.size() > res) {
-                res = resArray.size();
+                if (resArray.size() > res) {
+                    res = resArray.size();
+                }
             }
         }
         return res;
